2434b961a144074837d5c2ca83cfa24977c6d69e
Phfish1 <philip.skudvig@icloud.com>
2024-04-09 23:49:28 +0200

    dijkstras on a tree

--- Graph Theory/dijkstras/dijkstras_on_tree.py
+++ Graph Theory/dijkstras/dijkstras_on_tree.py
@@ -1,31 +1,25 @@
 from time import sleep
 
 matrix = [
-[ 1, 1, 11 ] ,
-[ 4, 5, 1 ] ,
-[ 7, 8, 9 ]
+[ 0 ] ,
+[ 2, 4 ] ,
+[ 0, 5, 6 ],
+[ 7, 2, 9, 10 ],
+[ 25, 11, 1, 0, 5 ],
+[ 1, 88, 51, 88, 61, 4 ],
+[ 93, 12, 73, 36, 71, 65, 34 ],
+[ 233, 5, 2, 1, 6, 7, 55, 1 ],
+[ 16, 111, 213, 9, 23, 433, 1, 34, 13 ],
+[ 5, 23, 453, 789, 123, 200, 212, 345, 556, 99 ]
 ]
 
 
-matrix = [
-[ 272, 362, 965, 995, 603, 909, 758, 390, 709, 693 ] ,
-[ 665, 600, 806, 201, 905, 688, 294, 860, 6, 501 ] ,
-[ 362, 454, 902, 479, 632, 78, 469, 255, 650, 755 ] ,
-[ 337, 927, 535, 858, 839, 236, 813, 457, 360, 482 ] ,
-[ 153, 742, 367, 793, 749, 870, 413, 81, 961, 165 ] ,
-[ 937, 88, 291, 35, 325, 580, 912, 15, 777, 779 ] ,
-[ 813, 638, 641, 918, 140, 758, 755, 522, 745, 12 ] ,
-[ 902, 978, 273, 9, 80, 498, 850, 863, 651, 123 ] ,
-[ 262, 104, 32, 735, 780, 177, 327, 175, 667, 128 ] ,
-[ 45, 344, 622, 627, 349, 184, 802, 400, 701, 550 ]
-]
-
-def draw_maze(maze, row, column, path):
+def draw_maze(maze, row, column, path, fast):
     maze_string = ""
 
     for i in range(0, len(maze)):
         for j in range(0, len(maze[i])):
-            if ( (row == len(maze)-1 and column == len(maze[0])-1) and (i == len(maze)-1 and j == len(maze[0])-1) ) :
+            if ( row == len(maze)-1 and i == row and j == column):
                 maze_string += f"\033[92m{maze[i][j]}\033[00m, "
             elif (row == i and column == j):
                 maze_string += f"\033[91m{maze[i][j]}\033[00m, " # Marks current Vertex
@@ -38,32 +32,27 @@ def draw_maze(maze, row, column, path):
 
         maze_string += f"\n"
     
-
-    #print(f"{maze_string}", end='\r')
-    #sleep(0.000000001)
-    #for line in maze_string.splitlines():
-    #    print("\033[1A", end="\x1b[2K")
-
-    
-    #sleep(0.2)
-    print(f"{maze_string}")
-    print("---------------------")
+    if fast:
+        print(f"{maze_string}", end='\r')
+        sleep(0.001)
+        for line in maze_string.splitlines():
+            print("\033[1A", end="\x1b[2K")
+    else:
+        print(f"{maze_string}")
+        print("---------------------")
 
 def find_neighbours(graph, row, column):
-    ### Checks if index exists
-    ### Row vertecies represents the direction (DOWN) and the column_vertecies the direction (RIGHT)
-    ### THEN we return valid_paths AKA all the directions we can move in from the specified vertecy
 
-    row_vertecies = [+1, 0]
-    column_vertecies = [0, +1]
+    row_vertecies = [+1, +1, +1]
+    column_vertecies = [0, +1, -1]
 
     valid_paths = []
 
-    for direction in range(0, 2):
+    for direction in range(0, 3):
         next_row = row + row_vertecies[direction]
         next_column = column + column_vertecies[direction]
 
-        if ( (0 <= next_row < len(graph)) and (0 <= next_column < len(graph[0])) ):
+        if ( (0 <= next_row < len(graph)) and (0 <= next_column < len(graph[row])) ):
             valid_paths.append([next_row, next_column])
 
     return valid_paths
@@ -73,23 +62,24 @@ def dijkstras(graph, row, column): ### Row + Collumn representing the vertex
     global current_path
     global shortest_path
     
-    if row == (len(graph) -1) and column == (len(graph[0]) -1): ### Returns True when last vertex in maze is hit
+
+    if row == (len(graph) -1): ### Returns True when last vertex in maze is hit
         if current_cost < shortest_path["cost"]: ### Change from "<" to ">" for highest cost
             shortest_path["path"] = current_path.copy() ### Use copy, OR else shortest_path["path"] will be BOUND to the current_path
             shortest_path["cost"] = current_cost
+            print("NEW: ", shortest_path)
         return True
     
     next_paths = find_neighbours(graph, row, column)
 
-    
     for path in next_paths: ### Explores every path possible, but if there are no more vertixes to visit go back (recursivly) until you can visit one or if paths are depleted
         current_path.append( [path[0], path[1]] )
         current_cost += graph[current_path[-1][0]][current_path[-1][1]]
 
 
-        #draw_maze(graph, path[0], path[1], current_path)
+        draw_maze(graph, path[0], path[1], current_path, True)
 
-        if current_cost > shortest_path["cost"]:
+        if current_cost > shortest_path["cost"]: ### Makes dijkstras Eager
             current_cost -= graph[current_path[-1][0]][current_path[-1][1]]
             current_path.pop()
         else:
@@ -118,5 +108,11 @@ current_cost = matrix[0][0]
 
 result = dijkstras(matrix, 0, 0)
 
-draw_maze(matrix, 0, 0, result["path"])
+
+#print(matrix[-1][-1])
+#print(result)
+draw_maze(matrix, 0, 0, result["path"], False)
 print(result)
+
+### What more needs to change:
+### The find neighbours need to only return DOWN, DOWN-LEFT, DOWN-RIGHT.
